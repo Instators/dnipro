0ccf0eb36bab6b52bff9fa602cb19ab1befb71d3
Web3 Supporter <Jayepaul81@gmail.com>
2026-06-13 00:42:21 +0100

    Simplify next.config.js - remove unused/broken polyfill config

--- web/next.config.js
+++ web/next.config.js
@@ -1,25 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@dnipro/sdk'],
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      os: false,
-      path: false,
-      crypto: require.resolve('crypto-browserify'),
-      stream: require.resolve('stream-browserify'),
-      buffer: require.resolve('buffer'),
-    };
-    config.plugins.push(
-      new (require('webpack').ProvidePlugin)({
-        Buffer: ['buffer', 'Buffer'],
-        process: 'process/browser',
-      })
-    );
-    return config;
-  },
   images: {
     domains: ['raw.githubusercontent.com', 'assets.coingecko.com'],
   },
